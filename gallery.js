e6d7f81b5b3b48a35a0f8ffa81cb129188065e94
Hunter <hvanlear@gmail.com>
2020-06-06 11:39:29 -0400

    modal responseive

--- gallery.js
+++ gallery.js
@@ -1,10 +1,13 @@
 const current = document.querySelector("#imagepreview");
 const imgs = document.querySelectorAll(".imgs img");
+const title = document.querySelectorAll("h5#exampleModalLabel.modal-title");
 
 imgs.forEach((img) =>
   img.addEventListener("click", (e) => {
+    let imgTitle = e.target.alt;
     e.preventDefault();
 
+    title[0].innerHTML = `${imgTitle}`;
     current.src = e.target.src;
   })
 );
